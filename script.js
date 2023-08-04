3f49c4ce12ccb0612183064cc29044964c01ff27
Shivam Kumar <119694506+kumarshivam04203@users.noreply.github.com>
2023-08-05 03:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const countValue = document.querySelector('#counter');
+
+const increment = () => {
+// ......get the value from UI
+    let value = parseInt(countValue.innerText);
+// ......update the value
+    value = value + 1;
+//....... set the value onto UI
+    countValue.innerText = value;
+}
+
+const decrement = () => {
+
+    let value = parseInt(countValue.innerText);
+
+    value = value - 1;
+
+    countValue.innerText = value;
+}
+
+
